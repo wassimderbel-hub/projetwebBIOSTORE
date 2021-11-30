98ef02db6920f1d8b7aba92e35250476c84e42b9
wassimderbel-hub <76130281+wassimderbel-hub@users.noreply.github.com>
2021-11-30 20:13:46 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    var menubar = $('.menu-bar'),
-        scroll = $(window).scrollTop();
-  
-    if (scroll >= 100) menubar.addClass('fixed');
-    else menubar.removeClass('fixed');
-  });
